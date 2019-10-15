c953404beed99c42c338aee380b018bab4880047
safjammed <safayatjamil27@gmail.com>
2019-10-16 01:33:58 +0600

    react hot reload and code splitting added

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,10 @@
 const mix = require('laravel-mix');
+require('laravel-mix-alias');
+require('laravel-mix-bundle-analyzer');
 
+mix.alias({
+    '@app': '/resources/js',
+});
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,5 +16,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.react('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css').extract().version();
+// mix.browserSync('127.0.0.1:8000');
+if (!mix.inProduction()) {
+    mix.bundleAnalyzer();
+}
