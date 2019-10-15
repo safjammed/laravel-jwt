29aee1a3de300f50316fac29ccd73d16d239afa7
safjammed <safayatjamil27@gmail.com>
2019-10-15 23:00:03 +0600

    JWT laravel 6.2 with react scaffolding includes hot reload and other goodies

---
+++ webpack.mix.js
@@ -0,0 +1,27 @@
+const mix = require('laravel-mix');
+require('laravel-mix-alias');
+require('laravel-mix-bundle-analyzer');
+
+mix.alias({
+    '@app': '/resources/js/src',
+    '@components' : '/resources/js/src/components',
+    '@c' : '/resources/js/src/components',
+    '@parts' : '/resources/js/src/parts',
+});
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.react('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css').extract().version();
+// mix.browserSync('127.0.0.1:8000');
+if (!mix.inProduction()) {
+    mix.bundleAnalyzer();
+}
